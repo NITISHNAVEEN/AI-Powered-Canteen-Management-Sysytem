244079ab282becd30a0a00b962ddedaf2fcb857a
Nitish Naveen <nitishnaveen5679@gmail.com>
2025-11-13 03:59:05 +0000

    no it is not, please add my realtime database: now use this as my realti

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:495481048992:web:fe938b80637b7875529ec5",
   "apiKey": "AIzaSyCc0B9pNH7le3bfhCT3fTDQGmsygBJnlcc",
   "authDomain": "studio-6225864195-5f36f.firebaseapp.com",
+  "databaseURL": "https://studio-6225864195-5f36f-default-rtdb.firebaseio.com",
   "measurementId": "",
   "messagingSenderId": "495481048992"
 };
