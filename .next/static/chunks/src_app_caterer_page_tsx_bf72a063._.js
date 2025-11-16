795c8874ce4fd5d36cfb38960b0da7e4302caef2
Nitish Naveen <nitishnaveen5679@gmail.com>
2025-11-16 16:17:44 +0000

    update 1

---
+++ .next/static/chunks/src_app_caterer_page_tsx_bf72a063._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1c0092d5._.js",
+  "static/chunks/node_modules_357f55b8._.js"
+],
+    source: "dynamic"
+});
