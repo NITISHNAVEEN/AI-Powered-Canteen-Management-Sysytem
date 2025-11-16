4d1caabae77e23f19c56fee5db064a87717066e0
Nitish Naveen <nitishnaveen5679@gmail.com>
2025-11-16 01:45:45 +0000

    remove any kind od association with olad firestore project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6225864195-5f36f",
-  "appId": "1:495481048992:web:fe938b80637b7875529ec5",
-  "apiKey": "AIzaSyCc0B9pNH7le3bfhCT3fTDQGmsygBJnlcc",
-  "authDomain": "studio-6225864195-5f36f.firebaseapp.com",
-  "databaseURL": "https://studio-6225864195-5f36f-default-rtdb.firebaseio.com",
-  "measurementId": "",
-  "messagingSenderId": "495481048992"
+  "projectId": "foodie-s-hub-9d2bc",
+  "appId": "1:350720188995:web:8674536d3c0b2b8347ce68",
+  "storageBucket": "foodie-s-hub-9d2bc.appspot.com",
+  "apiKey": "AIzaSyClgSc_3A3l3V3jBBlT92S_m_A9i-2A1_o",
+  "authDomain": "foodie-s-hub-9d2bc.firebaseapp.com",
+  "messagingSenderId": "350720188995",
+  "databaseURL": "https://foodie-s-hub-9d2bc-default-rtdb.firebaseio.com"
 };
